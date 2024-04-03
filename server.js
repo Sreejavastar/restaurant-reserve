85fb5df39780f275a3a0199b61fd534d6f1d4899
anomic30 <anomchakravorty3008@gmail.com>
2024-04-03 19:51:13 +0530

    fix: async issues

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./config/config.env" });
 import app from './app.js';
+import { dbConnection } from './database/dbConnection.js';
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+dbConnection().then(() => {
+    app.listen(process.env.PORT, ()=>{
+        console.log(`🗄️  Server is running on port ${process.env.PORT}`);
+    });
+})
